b66f0d8840235204a9b62917e126c08ec034d6ed
abel <abel.ticona@virtualisurg.com>
2025-06-02 18:13:23 -0300

    feature: create a window using gflw with green background

--- demoCpp/src/main.cpp
+++ demoCpp/src/main.cpp
@@ -1,18 +1,49 @@
-#include "number.h"
+#include "physics_api.h"
 #include <iostream>
-using namespace m_cpp_haptic;
+using namespace m_physics_lib;
+
+#define GLAD_GL_IMPLEMENTATION
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 int main() {
 
-    Number x = 5.;
-    Number y = 7.;
+    PhysicsApi p(0.1f);
 
-    Number z;
-    SumTwoNumbers(&z, &x, &y); // x + y
-    z.print();
+    GLFWwindow* window;
 
-    float w = Sum(4.f, 6.f);
-    std::cout<< "[c++] Sum(float, float): " << w << std::endl;
-    
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    glClearColor(0.0,0.5,0.0,1.0);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window)) {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
